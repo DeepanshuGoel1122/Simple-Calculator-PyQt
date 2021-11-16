03e798d0109ca7105bbc0f9aa6d0f2c1dccec307
DeepanshuGoel1122 <72638651+DeepanshuGoel1122@users.noreply.github.com>
2021-11-16 16:26:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Finalcalculator1.ui'
-#
 # Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
@@ -564,10 +545,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-
-# In[ ]:
-
-
-
 
